73aee92d05f004930188f75d441b268dcbb21871
venicode <vitoria_aparecida2011@hotmail.com>
2024-06-17 17:28:14 -0300

    feat: add query findMethodPaymentById

--- src/main/java/com/venitmarket/api/repositories/MethodPaymentRepository.java
+++ src/main/java/com/venitmarket/api/repositories/MethodPaymentRepository.java
@@ -3,5 +3,8 @@ package com.venitmarket.api.repositories;
 import com.venitmarket.api.domains.MethodPayment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MethodPaymentRepository extends JpaRepository<MethodPayment, Long> {
+    Optional<MethodPayment> findMethodPaymentById(Long id);
 }
