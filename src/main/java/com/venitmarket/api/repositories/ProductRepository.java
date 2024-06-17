c1cda683afc9c1854cef8b792d41d95cb6d8163e
venicode <vitoria_aparecida2011@hotmail.com>
2024-06-17 17:27:57 -0300

    feat: add query findProductById

--- src/main/java/com/venitmarket/api/repositories/ProductRepository.java
+++ src/main/java/com/venitmarket/api/repositories/ProductRepository.java
@@ -3,5 +3,8 @@ package com.venitmarket.api.repositories;
 import com.venitmarket.api.domains.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    Optional<Product> findProductById(Long id);
 }
