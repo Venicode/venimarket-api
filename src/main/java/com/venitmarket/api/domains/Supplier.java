bc2bb4f9a8402140e00ae16cdf8a16d1da49725a
venicode <vitoria_aparecida2011@hotmail.com>
2024-06-17 12:17:04 -0300

    feat: create Supplier domain

---
+++ src/main/java/com/venitmarket/api/domains/Supplier.java
@@ -0,0 +1,28 @@
+package com.venitmarket.api.domains;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.*;
+
+@Entity(name = "Supplier")
+@Table(name = "TB_SUPPLIERS")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Supplier {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String cnpj;
+    @Column(unique = true)
+    private String corporateName;
+    private String tradeName;
+    @Column(unique = true)
+    private String phone;
+    @Column(unique = true)
+    @Email
+    private String email;
+}
