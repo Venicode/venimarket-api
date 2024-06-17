55ada298ec6a9a9d7339b1a454a135cc290de3c4
venicode <vitoria_aparecida2011@hotmail.com>
2024-06-17 17:27:40 -0300

    feat: add query (not finished)

--- src/main/java/com/venitmarket/api/repositories/StockRepository.java
+++ src/main/java/com/venitmarket/api/repositories/StockRepository.java
@@ -2,6 +2,15 @@ package com.venitmarket.api.repositories;
 
 import com.venitmarket.api.domains.Stock;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface StockRepository extends JpaRepository<Stock, Long> {
+    @Modifying
+    @Query("""
+            update Stock s
+            set s.quantity = :quantity
+            where s.product.getIdProduct() = :idProduct
+            """)
+    void updateQuantity(Long idProduct, int quantity);
 }
