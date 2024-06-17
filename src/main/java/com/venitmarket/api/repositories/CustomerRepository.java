c52d68cf8f0ddd6af6a0015b4b8f4799cf336bf9
venicode <vitoria_aparecida2011@hotmail.com>
2024-06-17 17:29:12 -0300

    feat: add query findCustomerById

--- src/main/java/com/venitmarket/api/repositories/CustomerRepository.java
+++ src/main/java/com/venitmarket/api/repositories/CustomerRepository.java
@@ -3,5 +3,8 @@ package com.venitmarket.api.repositories;
 import com.venitmarket.api.domains.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Optional<Customer> findCustomerById(Long id);
 }
