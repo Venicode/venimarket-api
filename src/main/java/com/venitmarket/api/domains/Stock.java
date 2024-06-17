2fe97ca96a098026d44231520870077bb6863cca
venicode <vitoria_aparecida2011@hotmail.com>
2024-06-17 12:16:57 -0300

    feat: create Stock domain

---
+++ src/main/java/com/venitmarket/api/domains/Stock.java
@@ -0,0 +1,21 @@
+package com.venitmarket.api.domains;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity(name = "Stock")
+@Table(name = "TB_STOCK")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Stock {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @JoinColumn(name = "id_product")
+    @OneToMany
+    private Long id_Product;
+    private int quantity;
+}
