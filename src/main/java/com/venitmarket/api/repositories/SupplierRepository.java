0b285f4f2e49c27b2bacee9f1c4742441efc396a
venicode <vitoria_aparecida2011@hotmail.com>
2024-06-17 17:27:25 -0300

    feat: add query findSupplierById

--- src/main/java/com/venitmarket/api/repositories/SupplierRepository.java
+++ src/main/java/com/venitmarket/api/repositories/SupplierRepository.java
@@ -3,5 +3,8 @@ package com.venitmarket.api.repositories;
 import com.venitmarket.api.domains.Supplier;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface SupplierRepository extends JpaRepository<Supplier, Long> {
+    Optional<Supplier> findSupplierById(Long id);
 }
